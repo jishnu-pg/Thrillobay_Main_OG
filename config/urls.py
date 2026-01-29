8e9657ebc597f022a9bb7b1ea800fcf9198c6f2c
Zarco0 <pgjishnu10@gmail.com>
2026-01-29 18:11:33 +0530

    Add root URL welcome message

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({
+        "message": "Welcome to Thrillobay API",
+        "status": "Running",
+        "documentation": "/api/",
+        "admin": "/admin/"
+    })
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path("api/", include("api.urls")),
 ]
